9a45c5c51077b2542a65827ec927af7c1c02a44d
Gopalakrishna0001 <120792723+Gopalakrishna0001@users.noreply.github.com>
2023-06-02 16:11:33 +0530

    import java.util.*; public class solution {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         String str=sc.nextLine();         String str1="Hello Technicalhub";         System.out.println(str1);         String str2=str;         System.out.println(str2);     } }

---
+++ Hello_Technicalhub.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class solution
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+        String str1="Hello Technicalhub";
+        System.out.println(str1);
+        String str2=str;
+        System.out.println(str2);
+    }
+}
